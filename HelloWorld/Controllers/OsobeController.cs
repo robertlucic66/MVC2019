faa741cd7d4489b0880939f356bb13d02ce1ece2
Robert Lucić <robertlucic66@outlook.com>
2019-03-29 18:40:11 +0100

    OsobeController

---
+++ HelloWorld/Controllers/OsobeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class OsobeController : Controller
+    {
+        // GET: Osobe/PopuniOsobu
+        public ActionResult PopuniOsobu()
+        {
+            return View();
+        }
+
+        // POST: Osobe/PopuniOsobu
+        [HttpPost]
+        public ActionResult PrikaziOsobu(Osoba osoba)
+        {
+            return View(osoba);
+        }
+
+
+    }
+}
