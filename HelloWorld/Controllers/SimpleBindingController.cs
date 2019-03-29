2dedc6dbe8852467b32cba9d9a7b2c32f891ab04
Robert Lucić <robertlucic66@outlook.com>
2019-03-29 17:54:21 +0100

    primjer 4.1.1

---
+++ HelloWorld/Controllers/SimpleBindingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class SimpleBindingController : Controller
+    {
+        public ViewResult SimpleBindMetoda()
+        {
+            return View("SimpleBindMetoda");
+        }
+
+        [HttpPost]
+
+        public ViewResult SimpleBindMetoda(string ime)
+        {
+            string pozdrav = "Pozdrav, " + ime + "!";
+            return View("SimpleBindMetoda", (object)pozdrav);
+        }
+
+    }
+}
