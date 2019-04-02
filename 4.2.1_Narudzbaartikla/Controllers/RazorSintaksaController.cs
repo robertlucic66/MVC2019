afc7023e7d38a1a7ea9832943da483c5b4b4c48a
WorldArtGames <wag.head.office@gmail.com>
2019-04-02 16:26:34 +0200

    Razor for, foreach, while gore-dolje

---
+++ 4.2.1_Narudzbaartikla/Controllers/RazorSintaksaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4._2._1_Narudzbaartikla.Controllers
+{
+    public class RazorSintaksaController : Controller
+    {
+        // GET: RazorSintaksa
+        public ViewResult SwitchView()
+        {
+            return View();
+        }
+
+
+        public ViewResult ViewZaForPetlju()
+        {
+            string[] voce = new string[]
+            {
+                "Jabuka",
+                "Kruška",
+                "Banana",
+                "Grožđe",
+                "Šljiva"
+            };
+            return View(voce);
+        }
+    }
+}
