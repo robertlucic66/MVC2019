07fb824fa0f752cbd41335dfc5df20ed66585700
WorldArtGames <wag.head.office@gmail.com>
2019-04-03 18:17:27 +0200

    FormHelper

---
+++ 4.2.1_Narudzbaartikla/Controllers/HtmlHelperController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace _4._2._1_Narudzbaartikla.Controllers
+{
+    public class HtmlHelperController : Controller
+    {
+        // GET: HtmlHelper
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private string[] mjesta = new string[]
+        {
+            "Split", "Osijek", "Zadar", "Rijeka"
+        };
+        
+
+        public ViewResult FormHelper()
+        {
+            ViewBag.Mjesta = this.mjesta;
+            return View(new Osoba());
+        }
+
+        [HttpPost]
+        public ViewResult FormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = this.mjesta;
+            ViewBag.Poruka = "sdagfsadf";
+            return View(new Osoba());
+        }
+
+    }
+}
