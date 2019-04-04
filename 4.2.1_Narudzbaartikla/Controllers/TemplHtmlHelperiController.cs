b1e7e35f9248808332db6dcbb8df0960f82fecaa
Robert Lucić <robertlucic66@outlook.com>
2019-04-04 17:16:02 +0200

    6.2 Templated helper methods

---
+++ 4.2.1_Narudzbaartikla/Controllers/TemplHtmlHelperiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _4._2._1_Narudzbaartikla.Models;
+
+namespace _4._2._1_Narudzbaartikla.Controllers
+{
+    public class TemplHtmlHelperiController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ViewResult HtmlEditorView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+        public ViewResult EditorModelView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult EditorModelView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
---
+++ 4.2.1_Narudzbaartikla/Models/OsobaTempl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _4._2._1_Narudzbaartikla.Models
+{
+    public class OsobaTempl
+    {
+        public int OsobaId { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string MjestoStanovanja { get; set; }
+        public string Spol { get; set; }
+        public bool Zaposlen { get; set; }
+        public DateTime? DatumRodjenja { get; set; }
+        public string Napomena { get; set; }
+        public Adresa AdresaOsobe { get; set; }
+
+
+
+
+
+    }
+}
