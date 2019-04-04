69b71f18acfe3b8b11f921b965d16c3fc2c1c88a
Robert Lucić <robertlucic66@outlook.com>
2019-04-04 18:28:10 +0200

    Meta Data View

---
+++ 4.2.1_Narudzbaartikla/Controllers/MetaDataHtmlHelperController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _4._2._1_Narudzbaartikla.Models;
+
+namespace _4._2._1_Narudzbaartikla.Controllers
+{
+    public class MetaDataHtmlHelperController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ViewResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+
+        [HttpPost]
+        public ViewResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+
+    }
+}
