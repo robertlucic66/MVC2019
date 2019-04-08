a04a94092e8b466d0ed24144ba70566c35c08d68
Robert Lucić <robertlucic66@outlook.com>
2019-04-08 18:03:38 +0200

    ado.net

---
+++ ado_net_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ado_net_spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject direktno
+        public ActionResult Index()
+        {
+            // Prvo kreiramo conn string
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            // Nakon toga instanca Sqlconnection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if(conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write("Greška spajanja na bazu: " + sqlEx.ToString());
+            }
+            catch (Exception ex)
+            {
+                Response.Write("neka greška" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return View();
+        }
+
+        // GET: SqlConnectionObject preko Web.config
+        public ActionResult SpojPrekoWebConfig()
+        {
+            // Prvo kreiramo conn string
+            //string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+            // Nakon toga instanca Sqlconnection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Response.Write("Greška spajanja na bazu: " + sqlEx.ToString());
+            }
+            catch (Exception ex)
+            {
+                Response.Write("neka greška" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return View();
+        }
+    }
+}
