d18881ae307f5b8fb323de201ce1ad66a8d63d0b
Robert Lucić <robertlucic66@outlook.com>
2019-04-12 17:52:38 +0200

    napravljen cart controller / index

---
+++ WebShop/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private WebShopEntities db = new WebShopEntities();
+        public static List<Proizvodi> lstProizvodi = new List<Proizvodi>;
+        // GET: Cart
+        public ActionResult Index()
+        {
+            if(Session["Cart"] != null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+            return View();
+        }
+    }
+}
